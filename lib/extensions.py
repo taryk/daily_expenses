557e6689ec36c49f7e5214a57f93ee97fb2a21ff
Taras Iagniuk <mrtaryk@gmail.com>
2016-06-11 22:38:02 +0300

    Use in-memory database for unit tests.

--- lib/extensions.py
+++ lib/extensions.py
@@ -1,9 +1,14 @@
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from models import Base
 
-engine = create_engine('sqlite:///db/daily_expenses.db', echo=True)
+# Use in-memory database when testing stuff.
+DB_FILE = '/db/daily_expenses.db' \
+    if not hasattr(sys, '_called_from_test') else ''
+
+engine = create_engine('sqlite://' + DB_FILE, echo=True)
 
 Base = declarative_base(cls=Base)
 
@@ -11,7 +16,9 @@ Session = sessionmaker(bind=engine)
 db = Session()
 Base.__class__.db = db
 
+
 def init_db():
-    # import models
-    # Base.metadata.create_all(engine)
-    print("init db")
+    """Creates new DB tables if called from tests.
+    """
+    if hasattr(sys, '_called_from_test'):
+        Base.metadata.create_all(engine)
